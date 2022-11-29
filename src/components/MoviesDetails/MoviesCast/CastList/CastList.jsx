9e2f64eb0eb47a2eac798972a79a4c6b2d1be948
InnaShtondenko <inna.mycase@gmail.com>
2022-11-29 18:38:34 +0200

    Create CastList.jsx

---
+++ src/components/MoviesDetails/MoviesCast/CastList/CastList.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import {
+    StyledCastList,
+    CastImage,
+    CastItem,
+    ArtistName,
+    Character,
+} from './CastList.styled'; 
+import noImage from 'images/no-images.png';
+
+export const CastList = ({ castData }) => (
+  <StyledCastList>
+    {castData.map(({ cast_id, name, profile_path, character }) => (
+      <CastItem key={cast_id}>
+        <CastImage
+          src={
+            profile_path
+              ? `https://image.tmdb.org/t/p/w342${profile_path}`
+              : noImage
+          }
+          alt={name}
+          width={230}
+          height={345}
+        />
+        <ArtistName>{name}</ArtistName>
+        <Character>
+          {character ? `Character: ${character}` : `Character not specified`}
+        </Character>
+      </CastItem>
+    ))}
+  </StyledCastList>
+);
+
+CastList.propTypes = {
+  castData: PropTypes.array.isRequired,
+};
